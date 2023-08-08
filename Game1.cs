bcc79e52aec3d21152a61e362fc41382caa98ff7
maross3 <maross334@gmail.com>
2023-08-08 19:43:25 -0400

    added liquid visuals

--- Game1.cs
+++ Game1.cs
@@ -13,6 +13,12 @@ namespace Physics
         private static readonly List<Particle> _SParticles = new();
         public static Rectangle ScreenBounds { get; private set; }
 
+        private Vector2 _upForce = new(0, -30 * PIXELS_PER_METER);
+        private Vector2 _leftForce = new(-30 * PIXELS_PER_METER, 0);
+        private Vector2 _rightForce = new(30 * PIXELS_PER_METER, 0);
+        private Vector2 _downForce = new(0, 30 * PIXELS_PER_METER);
+        
+        private Rectangle _liquid;
         
         public Game1()
         {
@@ -29,21 +35,36 @@ namespace Physics
             var bigParticle = new Particle(150, 100, 10.0f, 20);
            _SParticles.Add(particle);
            _SParticles.Add(bigParticle);
-
-
            
             base.Initialize();
         }
-
+        private Texture2D _waterTexture;
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            
             foreach (var particle in _SParticles) particle.Initialize(GraphicsDevice);
             ScreenBounds = GraphicsDevice.Viewport.Bounds;
-           liquid.X = 0;
-           liquid.Y = ScreenBounds.Height / 2;
-           liquid.Width = ScreenBounds.Width;
-           liquid.Height = ScreenBounds.Height / 2;
+            
+            // sets the bottom half of the screen as the liquid
+           _liquid.X = 0;
+           _liquid.Y = ScreenBounds.Height / 2;
+           _liquid.Width = ScreenBounds.Width;
+           _liquid.Height = ScreenBounds.Height / 2;
+           
+            CreateWaterTexture();
+        }
+
+        private void CreateWaterTexture()
+        {
+            _waterTexture = new Texture2D(GraphicsDevice, _liquid.Width, _liquid.Height);
+            var numPixels = _liquid.Width * _liquid.Height;
+            var colorData = new Color[numPixels];
+
+            for (var i = 0; i < numPixels; i++)
+                colorData[i] = Color.White;
+
+            _waterTexture.SetData(colorData);
         }
 
         #endregion
@@ -57,6 +78,7 @@ namespace Physics
             _spriteBatch.Begin();
             
             // draw calls
+            _spriteBatch.Draw(_waterTexture, _liquid, Color.Blue); 
             DrawParticles();
             
             _spriteBatch.End();
@@ -69,24 +91,28 @@ namespace Physics
         #endregion
 
         #region updating
-        private Vector2 UpForce = new(0, -30 * PIXELS_PER_METER);
-        private Vector2 LeftForce = new(-30 * PIXELS_PER_METER, 0);
-        private Vector2 RightForce = new(30 * PIXELS_PER_METER, 0);
-        private Vector2 DownForce = new(0, 30 * PIXELS_PER_METER);
-        private Rectangle liquid;
+
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
                 Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            
+            // handles mouse input, spawns a new particle when the mouse is clicked.
+            if (Mouse.GetState() is {LeftButton: ButtonState.Pressed})
+            {
+                var newParticle = new Particle(Mouse.GetState().X, Mouse.GetState().Y, 1.0f, 8);
+                newParticle.Initialize(GraphicsDevice);
+                _SParticles.Add(newParticle);
+            }
 
             DeltaUpdate(gameTime);
             foreach (var particle in _SParticles)
             {
-                if (Keyboard.GetState().IsKeyDown(Keys.Up)) particle.AddForce(UpForce);
-                else if (Keyboard.GetState().IsKeyDown(Keys.Left)) particle.AddForce(LeftForce);
-                else if (Keyboard.GetState().IsKeyDown(Keys.Right)) particle.AddForce(RightForce);
-                else if (Keyboard.GetState().IsKeyDown(Keys.Down)) particle.AddForce(DownForce);
+                if (Keyboard.GetState().IsKeyDown(Keys.Up)) particle.AddForce(_upForce);
+                else if (Keyboard.GetState().IsKeyDown(Keys.Left)) particle.AddForce(_leftForce);
+                else if (Keyboard.GetState().IsKeyDown(Keys.Right)) particle.AddForce(_rightForce);
+                else if (Keyboard.GetState().IsKeyDown(Keys.Down)) particle.AddForce(_downForce);
                 particle.Update(gameTime);
             }
             base.Update(gameTime);
@@ -103,7 +129,7 @@ namespace Physics
                 particle.AddForce(wind * PIXELS_PER_METER);
                 particle.AddForce(weight);
                 
-                if (particle.position.Y >= liquid.Y)
+                if (particle.position.Y >= _liquid.Y)
                     particle.AddForce(Force.GenerateDragForce(particle, 0.01f));
 
                 particle.DeltaUpdate(deltaTime);
